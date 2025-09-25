3f5a9d436867eb74164dd67569e51af8e5aa2ee5
Wahaj Khan <wahaj khan@shiwantek.local>
2025-09-25 18:23:22 +0500

    link header

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TopHeader from "../Header/TopHeader";
 import Navbar from "../Header/Navbar";
+import LinkHeader from "../Header/LinkHeader";
 const Header = () => {
   return (
     <div>
       <TopHeader />
       <Navbar />
+      <LinkHeader/>
     </div>
   );
 };
