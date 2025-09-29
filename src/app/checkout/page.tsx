f8c815f299756f81dba77993b0eb425998ca5af9
Wahaj Khan <wahaj khan@shiwantek.local>
2025-09-30 01:43:15 +0500

    changes

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CheckoutComponent from '../components/CheckoutComponent/CheckoutComponent'
+
+const page = () => {
+  return (
+    <div>
+        <CheckoutComponent/>
+    </div>
+  )
+}
+
+export default page
