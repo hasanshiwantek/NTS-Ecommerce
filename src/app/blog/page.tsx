4984b3e35e797a03f86453b870071f6a3ea3fafb
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-10-09 20:37:38 +0500

    Make Single Blog Page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,7 @@ import BlogSidebar from "../components/SingleBlog/BlogSidebar.tsx/BlogSidebar";
 const page = () => {
   return (
     <>
-      <main className="flex gap-10 flex-row  w-full xl:max-w-[1290px] 2xl:max-w-[1720px]">
+      <main className="flex gap-11 flex-row  w-full xl:max-w-[1290px] 2xl:max-w-[1720px]">
           <SingleBlog />
           <BlogSidebar />
       </main>
