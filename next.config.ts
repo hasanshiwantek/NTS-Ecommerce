1255438d08c7691c7d70bedf8165edef761b13fa
Areeb Hussain <areeb.hussain@shiwantek.com>
2026-03-27 09:34:01 -0700

    next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   compress: true,
 
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -45,12 +46,12 @@ const nextConfig: NextConfig = {
         hostname: "*",
         pathname: "/**",
       },
-   {
+      {
         protocol: 'http',
         hostname: '3.12.69.112',
         pathname: '/**',
       },
-         {
+      {
         protocol: 'http',
         hostname: 'backend.sparemicro.com',
         pathname: '/**',
@@ -100,7 +101,7 @@ const nextConfig: NextConfig = {
     ];
   },
 
-      async rewrites() {
+  async rewrites() {
     return [
       {
         source: "/robots.txt",
@@ -109,7 +110,7 @@ const nextConfig: NextConfig = {
     ];
   },
 
-  
+
   webpack(config, { dev, isServer }) {
     // Target modern JS in client build
     if (!dev && !isServer) {
